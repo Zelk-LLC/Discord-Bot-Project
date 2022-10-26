ff4a0bf7c521594339afeed24b175f97da3510b9
AlejandroRuizZ <a01611451@itesm.mx>
2022-10-26 15:16:46 -0500

    Pay command started, conditions still missing.

---
+++ commands/pay.js
@@ -0,0 +1,62 @@
+const { SlashCommandBuilder, PermissionsBitField,PermissionFlagsBits } = require('discord.js');
+const {db} = require('../firebaseConfig.js')
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('pay')
+		.setDescription("Pay a given Ammount to user from your own wallet.")
+		.addUserOption(option =>
+			option.setName("user-tag")
+			.setDescription("User's @")
+			.setRequired(true))
+			.addIntegerOption(option =>
+				option.setName("ammount")
+			.setDescription("ammount")
+			.setRequired(true)),
+		
+	async execute(interaction) {
+		const userId = interaction.options.getUser("user-tag").id;
+		const ammount = interaction.options.getInteger("ammount")
+		db.collection("users")
+		.where("discordId","==",userId)
+		.get()
+		.then((QuerySnapshot) =>{
+			if(QuerySnapshot.empty){
+				interaction.reply("That user does not exist in our database.")
+			}
+			else{
+				QuerySnapshot.forEach((doc) => {
+                    db.collection("users")
+                    .doc(doc.id)
+                    .update({
+                        balance: doc.data().balance + ammount
+                    }).then(() => {
+                        db.collection('users')
+                        .where("discordId","==",interaction.user.id)
+                        .get()
+                        .then((QuerySnapshot) =>{
+                            QuerySnapshot.forEach((doc) => {
+                                db.collection("users")
+                                .doc(doc.id)
+                                .update({
+                                    balance: doc.data().balance - ammount
+                                })
+                            })
+                        }).catch((error) => {
+                            console.log("Error getting documents: ", error);
+                        });
+                        VerificationString = `User ${interaction.options.getUser('user-tag').username} has been paid ${ammount} Scrip.`
+                        interaction.reply(VerificationString)
+                    })
+                    .catch((error) => {
+                        console.error("Error adding document: ", error);
+                    });	
+                })
+			}
+			
+		}).catch((error) => {
+			console.log("Error getting documents: ", error);
+		});
+	},
+};
