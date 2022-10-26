dc32014deb1796ff853c048d88c6a6fd5fda6119
AlejandroRuizZ <a01611451@itesm.mx>
2022-10-26 11:59:55 -0500

    changes

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,26 +1,32 @@
-const {initializeApp} = require('firebase/app');
-const firebase = require('firebase')
-require('firebase/firestore')
-const {getFirestore} = require('firebase/firestore');
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
 
+// Import the functions you need from the SDKs you need
+const { initializeApp } = require("firebase/app");
+const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDxyvBI3C8neg-yoc2lwrPdJtOnNfMH2iE",
-    authDomain: "scrip-database.firebaseapp.com",
-    projectId: "scrip-database",
-    storageBucket: "scrip-database.appspot.com",
-    messagingSenderId: "44909120808",
-    appId: "1:44909120808:web:eee4ed91540c49d9bc141d",
-    measurementId: "G-E6PTD9KJK6"
-}
+  apiKey: "AIzaSyDxyvBI3C8neg-yoc2lwrPdJtOnNfMH2iE",
+  authDomain: "scrip-database.firebaseapp.com",
+  projectId: "scrip-database",
+  storageBucket: "scrip-database.appspot.com",
+  messagingSenderId: "44909120808",
+  appId: "1:44909120808:web:eee4ed91540c49d9bc141d",
+  measurementId: "G-E6PTD9KJK6"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-const db = firestore.firestore();
 module.exports = {
-    firebaseConfig: firebaseConfig,
-    app: app,
-    db: db
- }
+    firebaseConfig: firebaseConfig
+    ,app: app
+    ,db: db
 
-
+};
