7831cdbf6537923c2b57dac7b3bc46d59ce250d7
Zackery Bellerose <zbellerose@hcbrands.com>
2022-10-28 08:40:09 -0400

    Added firebaseContext to handle database queries

---
+++ Data/FirebaseContext.js
@@ -0,0 +1,42 @@
+import { db } from '../firebaseConfig.js'
+
+/**
+ * Updates the user's balance in the database
+ * @param {*} userId users's discord ID
+ * @param {*} amount amount to update the user's balance by
+ */
+const updateBalance = async (userId, amount) => {
+    // query the database to find the user
+    const user = await db.collection('users').where('discordId', '==', userId).get()
+    // if the user is not found, return
+    if (user.empty) return
+    // if the user is found, update their balance
+    user.forEach(doc => {
+        db.collection('users').doc(doc.id).update({
+        balance: doc.data().balance + amount
+        })
+    })
+}
+
+/**
+ * Adds a new user to the database
+ * @param {*} userId user's discord ID
+ * @param {*} initialBalance initial starting balance of the user
+ */
+const addUser = async (userId, initialBalance) => {
+    // query the database to find the user
+    const user = await db.collection('users').where('discordId', '==', userId).get()
+    // if the user is found, return
+    if (!user.empty) return
+    // if the user is not found, add them to the database
+    db.collection('users').add({
+        discordId: userId,
+        balance: initialBalance,
+        monthFromNow: 'PLACEHOLDER NULL'
+    })
+}
+
+export default {
+    updateBalance,
+    addUser
+    }
--- commands/register.js
+++ commands/register.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder, PermissionsBitField,PermissionFlagsBits } = require('discord.js');
-const {db} = require('../firebaseConfig.js')
+import { db } from '../firebaseConfig.js'
  
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('register')
-		.setDescription("Registers you to the Database if you're not already in it."),
+		.setDescription("Registers you to the database if you're not already in it."),
 		
 	async execute(interaction) {
 		const userId = interaction.user.id
