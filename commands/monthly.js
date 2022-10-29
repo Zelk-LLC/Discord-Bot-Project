0396abd6645906073ee6b245192d9a0ce193a8ca
Zackery Bellerose <zbellerose@hcbrands.com>
2022-10-29 10:19:47 -0400

    Fixed Balance, Monthly and player-balance

--- commands/monthly.js
+++ commands/monthly.js
@@ -1,47 +1,43 @@
 const { SlashCommandBuilder } = require('discord.js');
 const {db} = require('../firebaseConfig.js')
 const { EmbedBuilder } = require('discord.js');
+const { getUser, updateBalance } = require('../Data/FirebaseContext.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('monthly')
-		.setDescription("Pay a specified Amount to user monthly."),
+		.setDescription("Pay a specified amount to user monthly."),
 		
 	async execute(interaction) {
         const CurrentTime = new Date();
         const MonthFromNow = new Date(CurrentTime);
+        const monthly = 1000;
         MonthFromNow.setMonth(MonthFromNow.getMonth() + 1)
-        db.collection('users')
-        .where('discordId' ,'==',interaction.user.id)
-        .get()
-        .then((QuerySnapshot) =>{
-            if(QuerySnapshot.empty){
-                interaction.reply("You do not exist in our Database.")
-            }else{
-                db.collection("users")
-                .where("discordId","==",interaction.user.id)
-                .get()
-                .then((QuerySnapshot) =>{
-                    QuerySnapshot.forEach((doc) =>{
-                        var dbDate = new Date(doc.data().monthFromNow)
-                        
-                        console.log(dbDate)
-                        if(dbDate - CurrentTime > 0){
-                            interaction.reply(`${interaction.user.username} you've already used this command this month.`)
-                        }
-                        else{
-                            db.collection('users')
-                            .doc(doc.id)
-                            .update({
-                                balance: doc.data().balance + 1000,
-                                monthFromNow: MonthFromNow.toString()
-                            }).then(() =>{
-                                interaction.reply(`You've recieved a total of 1000 Scrip as your monthly payment.`)
-                            })
-                        }
-                    })
+
+        const user = await getUser(interaction.user.id);
+        if(user == undefined){
+            return interaction.reply("You don't have an account. Please use /register to create one.");
+        }
+        else {
+            if(user.docs[0].data().monthFromNow == 'PLACEHOLDER NULL'){
+                updateBalance(interaction.user.id, monthly);
+                db.collection('users').doc(user.docs[0].id).update({
+                    monthFromNow: MonthFromNow
                 })
+                return interaction.reply(`You have been given ${monthly} scrip for the month.`);
             }
-        })
+            else {
+                if(user.docs[0].data().monthFromNow < CurrentTime){
+                    updateBalance(interaction.user.id, monthly);
+                    db.collection('users').doc(user.docs[0].id).update({
+                        monthFromNow: MonthFromNow
+                    })
+                    return interaction.reply(`You have been given ${monthly} scrip for the month.`);
+                }
+                else {
+                    return interaction.reply(`You have been given ${monthly} scrip for the month.`);
+                }
+            }
+        }
     }
 }
