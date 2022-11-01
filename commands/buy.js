d18f773f8c9e8d2b6f044d7c315f67fa86c8e2f4
Zackery Bellerose <zbellerose@hcbrands.com>
2022-10-31 22:09:56 -0400

    Buyable items!!!

--- commands/buy.js
+++ commands/buy.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, PermissionsBitField,PermissionFlagsBits } = require('discord.js');
 const {db} = require('../firebaseConfig.js')
 const { EmbedBuilder } = require('discord.js');
-const { updateBalance, getUser, getItem,addItemToUser } = require('../Data/FirebaseContext.js');
+const { updateBalance, getUser, getItem, addItemToUser } = require('../Data/FirebaseContext.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,6 +11,7 @@ module.exports = {
             option.setName("item")
             .setDescription("Name of the item you want to buy.")
             .setRequired(true)),
+
     async execute(interaction){
         const item = interaction.options.getString('item');
         const user = await getUser(interaction.user.id);
@@ -22,15 +23,28 @@ module.exports = {
         if(itemData == undefined){
             return interaction.reply(`Item ${item} doesn't exist.`);
         }
+
         if(itemData.docs[0].data().quantity <= 0){
             return interaction.reply(`Item ${item} is out of stock.`);
         }
+        // Check if the user already has the max amount of items
+
+        // get the subcollection inventory from user
+        const userInventory = await db.collection('users').doc(user.docs[0].id).collection('inventory').where('name', '==', item).get()
+        // if the user already has the item, update the quantity
+        if (!userInventory.empty) {
+            if(userInventory.docs[0].data().owned >= itemData.docs[0].data().maxPerUser){
+                return interaction.reply(`You already have the max amount of ${item}.`);
+            }
+        }
+
         if(user.docs[0].data().balance < itemData.docs[0].data().price){
             return interaction.reply("You don't have enough scrip to complete this transaction.");
         }
-     
+
+        await addItemToUser(interaction.user.id, item);
         updateBalance(interaction.user.id, -itemData.docs[0].data().price);
 
-        interaction.reply(`You have bought ${item} for ${itemData.price} scrip.`);
+        interaction.reply(`You have bought ${item} for ${itemData.docs[0].data().price} scrip.`);
     }
 };
