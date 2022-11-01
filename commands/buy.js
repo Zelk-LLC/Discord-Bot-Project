aaf26aeb7a869a692ab69962b6a1cb60a8383e32
ZelkieZelk <robinthemage1@gmail.com>
2022-10-31 19:01:44 -0600

    Buy Command partially implemented, maxPerUser check missing as well as pushing map.

--- commands/buy.js
+++ commands/buy.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, PermissionsBitField,PermissionFlagsBits } = require('discord.js');
 const {db} = require('../firebaseConfig.js')
 const { EmbedBuilder } = require('discord.js');
-const { updateBalance, getUser, getItem } = require('../Data/FirebaseContext.js');
+const { updateBalance, getUser, getItem,addItemToUser } = require('../Data/FirebaseContext.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,18 +14,23 @@ module.exports = {
     async execute(interaction){
         const item = interaction.options.getString('item');
         const user = await getUser(interaction.user.id);
+        const itemData = await getItem(item);
         if(user == undefined){
             return interaction.reply("You don't have an account.");
         }
-        const itemData = getItem(item);
+        
         if(itemData == undefined){
             return interaction.reply("Item doesn't exist.");
         }
-        if(itemData.quantity <= 0){
+        if(itemData.docs[0].data().quantity <= 0){
             return interaction.reply("Item is out of stock.");
         }
-        if(user.balance < itemData.price){
+        if(user.docs[0].data().balance < itemData.docs[0].data().price){
             return interaction.reply("You don't have enough scrip to complete this transaction.");
         }
+     
+        updateBalance(interaction.user.id, user.balance - itemData.price);
+
+        interaction.reply(`You have bought ${item} for ${itemData.price} scrip.`);
     }
 };
