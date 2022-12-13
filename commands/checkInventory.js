0ebd37cc554558a58bd74dfe474821212079652a
ZelkieZelk <robinthemage1@gmail.com>
2022-12-13 12:22:23 -0600

    Check Inventory of player implemented.

--- commands/checkInventory.js
+++ commands/checkInventory.js
@@ -1,21 +1,50 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const {db} = require('../firebaseConfig.js')
 const { EmbedBuilder } = require('discord.js');
-const { getUser } = require('../Data/FirebaseContext.js');
+const { getInventory,getUser } = require('../Data/FirebaseContext.js');
+const buttonPages = require('../Models/pagination.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('checkinventory')
-		.setDescription('Provides you with your own personal balance.')
+		.setDescription('Check a player inventory.')
+		.addUserOption(option =>
+			option.setName("user-tag")
+			.setDescription("User's @")
+			.setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
 		
 	async execute(interaction) {
-
+		const userId = interaction.options.getUser("user-tag").id;
+		const embeds = [];
+        const inventory = await getInventory(userId);
+        const pageCountMax = Math.ceil(inventory.docs.length / 25);
 		const user = await getUser(interaction.user.id);
-		if(user == undefined){
-			return interaction.reply("You don't have an account. Please use /register to create one.");
+
+		if(pageCountMax == 0){
+            return interaction.reply("This user has no items in their inventory.");
+        }
+
+		for(let i = 0; i < pageCountMax; i++){
+			const embed = new EmbedBuilder()
+			.setColor([68, 41, 37])
+			.setTitle('User inventory:')
+			.setAuthor({name:interaction.options.getUser('user-tag').username, iconURL:interaction.options.getUser("user-tag").avatarURL()})
+				.setDescription('Here is a list of all the items this player owns.')
+				.setTimestamp()
+				.setFooter({text:`Page ${i + 1} of ${pageCountMax.toString()}`});
+			const fields = [];
+			for(let j = 0; j < 25; j++){
+				if(inventory.docs[i * 25 + j] == undefined){
+					break;
+				}
+				fields.push({name: inventory.docs[i * 25 + j].data().name, value: `Owned: ${inventory.docs[i * 25 + j].data().owned}`});
+			}
+
+			embed.addFields(fields);
+			embeds.push(embed);
 		}
-		
-        return;
+
+		buttonPages(interaction, embeds);
 	} // end of execute
 };
